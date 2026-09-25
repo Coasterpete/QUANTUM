faad57104c4674a0ac43714451822d21c8ddcc8a
Coasterpete <83011124+Coasterpete@users.noreply.github.com>
2026-09-25 18:55:08 -0400

    feat(operations): M1 custom acceleration profiles for launch/brake devices

--- check_duplicates2.py
+++
@@ -1,9 +0,0 @@
-f = open('tests/CMakeLists.txt', 'r')
-content = f.read()
-f.close()
-idx = content.find('QuantumCoreSupportSerializationTests')
-while idx >= 0:
-    print('Found at:', idx)
-    print(content[max(0,idx-50):idx+200])
-    print('---')
-    idx = content.find('QuantumCoreSupportSerializationTests', idx+1)
